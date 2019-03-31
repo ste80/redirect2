78875b999d0c752c675e2df70179420ad2596e7d
Steven Chong <steven.chong@ubm.com>
2019-03-31 17:09:40 +0800

    support no param

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),document.body.appendChild((function(a){a.href=t,a.innerText='redirect';return a})(document.createElement('a')))</script></body></html>
+<!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),document.body.appendChild((function(a){a.href=t||'javascript:history.back()',a.innerText='Click here to continue';return a})(document.createElement('a')))</script></body></html>
