4b8bfc13aae2cbafd74d23f2f009f435e8a49234
Steven Chong <steven.chong@ubm.com>
2019-03-31 17:15:27 +0800

    add php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),document.body.appendChild((function(a){a.href=t||'javascript:history.back()',a.innerText='Click here to continue';return a})(document.createElement('a')))</script></body></html>
+<?php
+$query = $_SERVER["QUERY_STRING"];
+if ( !empty($query) ) {
+    echo $query;
+} else {
+?><!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),document.body.appendChild((function(a){a.href=t||'javascript:history.back()',a.innerText='Click here to continue';return a})(document.createElement('a')))</script></body></html><?php
+}
+?>
