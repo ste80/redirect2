af0fa7c2e60f535e26acc878e8dc55f6c4f78767
Steven Chong <steven.chong@ubm.com>
2019-04-02 10:09:37 +0800

    add support for relative url

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
+$referer = $_SERVER['HTTP_REFERER'];
+if ( !empty($referer) ) {
+    $url_array = parse_url($referer);
+    $schema = empty($url_array['scheme']) ? 'https' : $url_array['scheme'];
+    $port = empty($url_array['port']) || intval($url_array['port']) == 80 ? '' : ':' . $url_array['port'];
+    $referer = $schema . '://' . $url_array['host'] . $port;
+}
+
 $query = $_SERVER['QUERY_STRING'];
+
 if ( !empty($query) ) {
-    // $ref = '';
-
-    // if (substr($query, 0, 1) == '/' && !empty($_SERVER['HTTP_REFERER'])) {
-    //     $url_array = parse_url($_SERVER['HTTP_REFERER']);
-    //     $schema = empty($url_array['scheme']) ? 'https' : $url_array['scheme'];
-    //     $port = empty($url_array['port']) || intval($url_array['port']) == 80 ? '' ':' . $url_array['port'];
-    //     $ref = schema . '://' . $url_array['hostname'] . $port;
-    //     $query = $ref . $query;
-    // }
-
-    header('Location: ' . $query);
+    if (substr($query, 0, 1) == '/') {
+        header('Location: ' . $referer . $query);
+    } else {
+        header('Location: ' . $query);
+    }
 } else {
-?><!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),setTimeout(function(){document.body.appendChild((function(a){a.href=t||'javascript:history.back()',a.innerText='Click here to continue';return a})(document.createElement('a')))},1000)</script></body></html><?php
+?><!DOCTYPE html><html><body><script>(function(l,d){t=l.hash.substr(1);t&&l.replace(t);d.title=t;setTimeout(function(){a=d.createElement('a');a.href=(t||'javascript:history.back()').replace(/^\//,<?= json_encode($referer . '/') ?>);a.innerText='Click here to continue';d.body.appendChild(a)},1000)})(location,document)</script></body></html><?php
 }
 ?>
