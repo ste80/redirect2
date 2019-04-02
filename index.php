b96f2f7f736f337715c92767a6f0c565320517c4
Steven Chong <steven.chong@ubm.com>
2019-04-02 09:38:22 +0800

    support relative url

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 $query = $_SERVER['QUERY_STRING'];
 if ( !empty($query) ) {
+
+    if (substr($query, 0, 1) == '/' && !empty($_SERVER['HTTP_REFERER'])) {
+        $url_array = parse_url($_SERVER['HTTP_REFERER']);
+        $port = empty($url_array['port']) || int($url_array['port']) == 80 ? '' ':' . $url_array['port'];
+        $query = $url_array['scheme'] . '://' . $url_array['hostname'] . $port . $query;
+    }
+
     header('Location: ' . $query);
 } else {
 ?><!DOCTYPE html><html><body><script>t=location.hash.substr(1),t&&location.replace(t),(document.title=t),setTimeout(function(){document.body.appendChild((function(a){a.href=t||'javascript:history.back()',a.innerText='Click here to continue';return a})(document.createElement('a')))},1000)</script></body></html><?php
